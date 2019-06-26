ad97e47006009ffd8a78828fd32a3dc304524765
Mohammad Rahmani <rahmani@netlinks.ws>
2019-06-26 10:16:14 +0430

    Removed all Angular files

--- projects/ngx-skeleton-preloader/README.md
+++
@@ -1,5 +0,0 @@
-# NgxSkeletonPreloader
-
-A nice and ready to use Skeleton Preloader for Angular Applications
-
-
--- projects/ngx-skeleton-preloader/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-skeleton-preloader
- */
-
-export * from './lib/ngx-skeleton-preloader.service';
-export * from './lib/ngx-skeleton-preloader.component';
-export * from './lib/ngx-skeleton-preloader.module';
